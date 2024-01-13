29488f136f4d6653605ef40ff08c13e1ca3c910c
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2024-01-13 20:06:25 +0900

    init

---
+++ src/main/kotlin/com/coco/customcache/CustomcacheApplication.kt
@@ -0,0 +1,11 @@
+package com.coco.customcache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomcacheApplication
+
+fun main(args: Array<String>) {
+	runApplication<CustomcacheApplication>(*args)
+}
---
+++ src/test/kotlin/com/coco/customcache/CustomcacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coco.customcache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomcacheApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
