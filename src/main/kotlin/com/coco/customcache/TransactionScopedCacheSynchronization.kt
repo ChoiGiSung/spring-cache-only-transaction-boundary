441e4c836f4ad26c0f2199c7f0e7aa3e78efd726
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-01-15 23:07:22 +0900

    add transaction synchronization

---
+++ src/main/kotlin/com/coco/customcache/TransactionScopedCacheSynchronization.kt
@@ -0,0 +1,26 @@
+package com.coco.customcache
+
+import org.springframework.transaction.support.TransactionSynchronization
+import org.springframework.transaction.support.TransactionSynchronizationManager
+
+/**
+ *   It should be used when using the thread pool.
+ *   Delete the cache before it is returned because the KReadTransactionCache is stored in thread local.
+ */
+class TransactionScopedCacheSynchronization(val cache: TransactionScopedCache) : TransactionSynchronization {
+
+    override fun beforeCompletion() {
+        super.beforeCompletion()
+        if (TransactionSynchronizationManager.isActualTransactionActive()) {
+            TransactionSynchronizationManager.unbindResource(cache.name)
+        }
+    }
+
+    fun register() {
+        if (TransactionSynchronizationManager.isActualTransactionActive()) {
+            if (TransactionSynchronizationManager.getSynchronizations().none { it::class.java == this::class.java }) {
+                TransactionSynchronizationManager.registerSynchronization(this)
+            }
+        }
+    }
+}
